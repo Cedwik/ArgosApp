5ee6c454a5e68897d05970c5308b1a3a5fb1976f
Cedwik <Cedriclabrique@hotmail.fr>
2013-11-29 11:25:29 +0100

    Geolocation

--- js/script.js
+++ js/script.js
@@ -6,3 +6,26 @@
  
  
  
+ 
+ 
+ 
+ 
+ // Code pour afficher la carte 
+ 
+ function GoogleMap(){
+this.initialize = function(){
+var map = showMap();
+}
+
+var showMap = function(){
+var mapOptions = {
+zoom: 4,
+center: new google.maps.LatLng(poisiont.coords.latitude, position.coords.longitude),
+mapTypeId: google.maps.MapTypeId.ROADMAP
+}
+ 
+var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+ 
+return map;
+}
+}
