f25f8c0d18da2b2b99478c1d622a0d4260440b56
Cedwik <Cedriclabrique@hotmail.fr>
2013-12-05 15:04:53 +0100

    Test geolocalisation

--- js/script.js
+++ js/script.js
@@ -5,27 +5,58 @@
     $ cordova plugin rm org.apache.cordova.geolocation
  
  
- 
+ <uses-permission android:name="android.permission.ACCESS_COARSE_LOCATION" />
+<uses-permission android:name="android.permission.ACCESS_FINE_LOCATION" />
+<uses-permission android:name="android.permission.ACCESS_LOCATION_EXTRA_COMMANDS" />
  
  
  
  
  // Code pour afficher la carte 
  
- function GoogleMap(){
-this.initialize = function(){
-var map = showMap();
-}
+// Wait for device API libraries to load
+    //
+    document.addEventListener("deviceready", onDeviceReady, false);
 
-var showMap = function(){
-var mapOptions = {
-zoom: 4,
-center: new google.maps.LatLng(poisiont.coords.latitude, position.coords.longitude),
-mapTypeId: google.maps.MapTypeId.ROADMAP
-}
- 
-var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
- 
-return map;
-}
-}
+    // device APIs are available
+    //
+    function onDeviceReady() {
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);
+    }
+
+    // onSuccess Geolocation
+    //
+    function onSuccess(position) {
+        var element = document.getElementById('geolocation');
+        element.innerHTML = 'Latitude: '           + position.coords.latitude              + '<br />' +
+                            'Longitude: '          + position.coords.longitude             + '<br />' +
+                            'Altitude: '           + position.coords.altitude              + '<br />' +
+                            'Accuracy: '           + position.coords.accuracy              + '<br />' +
+                            'Altitude Accuracy: '  + position.coords.altitudeAccuracy      + '<br />' +
+                            'Heading: '            + position.coords.heading               + '<br />' +
+                            'Speed: '              + position.coords.speed                 + '<br />' +
+                            'Timestamp: '          + position.timestamp                    + '<br />';
+    }
+
+    // onError Callback receives a PositionError object
+    //
+    function onError(error) {
+        alert('code: '    + error.code    + '\n' +
+              'message: ' + error.message + '\n');
+    }
+
+	
+	
+	
+	
+	
+	// Google map api
+      function initialize() {
+        var mapOptions = {
+          center: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
+          zoom: 8
+        };
+        var map = new google.maps.Map(document.getElementById("map-canvas"),
+            mapOptions);
+      }
+      google.maps.event.addDomListener(window, 'load', initialize);
