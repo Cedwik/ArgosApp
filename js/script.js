3eb7139dae71468fff1fa4e1ae01312247604fd1
Cedwik <Cedriclabrique@hotmail.fr>
2013-12-07 16:25:30 +0100

    G TOUT CHNGER

--- js/script.js
+++ js/script.js
@@ -44,19 +44,3 @@
         alert('code: '    + error.code    + '\n' +
               'message: ' + error.message + '\n');
     }
-
-	
-	
-	
-	
-	
-	// Google map api
-      function initialize() {
-        var mapOptions = {
-          center: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
-          zoom: 8
-        };
-        var map = new google.maps.Map(document.getElementById("map-canvas"),
-            mapOptions);
-      }
-      google.maps.event.addDomListener(window, 'load', initialize);
