dcb79078b5dc77bee58aae2ff8bf653b32f6ff1b
Cedwik <Cedriclabrique@hotmail.fr>
2013-12-18 22:07:04 +0100

    test

--- js/index.js
+++ js/index.js
@@ -48,73 +48,90 @@ var app = {
     }
 	
 	
-	
+	function onDeviceReady() {
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);
+    }
+
+    // onSuccess Geolocation
+    //
+    function onSuccess(position) {
+        var element = document.getElementById('geolocation');
+        element.innerHTML = 'Latitude: '           + position.coords.latitude              + '<br />' +
+                            'Longitude: '          + position.coords.longitude             + '<br />' +
+                            'Altitude: '           + position.coords.altitude              + '<br />' +
+                            'Accuracy: '           + position.coords.accuracy              + '<br />' +
+                            'Altitude Accuracy: '  + position.coords.altitudeAccuracy      + '<br />' +
+                            'Heading: '            + position.coords.heading               + '<br />' +
+                            'Speed: '              + position.coords.speed                 + '<br />' +
+                            'Timestamp: '          + position.timestamp                    + '<br />';
+    }
+
+    // onError Callback receives a PositionError object
+    //
+    function onError(error) {
+        alert('code: '    + error.code    + '\n' +
+              'message: ' + error.message + '\n');
+    }
+
 	
 	// Mon code
-	function findMyLocation() {
-            //Check the network connection
-            var networkConnection = navigator.network.connection.type;
-            if (networkConnection != null) {
-                //Find your location
-                navigator.geolocation.getCurrentPosition(success, error);
-            }
-            else{
-                alert('Please check your network connection and try again.');
-            }
-        }
+	// function findMyLocation() {
+            // Check the network connection
+            // var networkConnection = navigator.network.connection.type;
+            // if (networkConnection != null) {
+                // Find your location
+                // navigator.geolocation.getCurrentPosition(success, error);
+            // }
+            // else{
+                // alert('Please check your network connection and try again.');
+            // }
+        // }
  
-        function success(position) {
-            var latitude = position.coords.latitude;
-            var longitude = position.coords.longitude;
-        }
+        // function success(position) {
+            // var latitude = position.coords.latitude;
+            // var longitude = position.coords.longitude;
+        // }
  
-        function error(error) {
-            alert('Some error occured please try again.');
-        }
+        // function error(error) {
+            // alert('Some error occured please try again.');
+        // }
          
-        function getDetails(latitude, longitude) {
-            var url = "http://maps.googleapis.com/maps/api/geocode/json?latlng="+latitude+","+longitude+"&sensor=false";
-            $.getJSON(url, function(data) {
-                var formatted_address = data['results'][0]['formatted_address'];
-                htmlData =  'Latitude : ' + latitude + '<br/>';
-                htmlData += 'Longitude : ' + longitude + '<br/>';
-                htmlData += 'Location : ' + formatted_address;
-                $("#location").html('<a href="#" class="btn" onclick="drawMap('+latitude+', '+longitude+');">'+htmlData+'</a>');
-            });
-        }
+        // function getDetails(latitude, longitude) {
+            // var url = "http://maps.googleapis.com/maps/api/geocode/json?latlng="+latitude+","+longitude+"&sensor=false";
+            // $.getJSON(url, function(data) {
+                // var formatted_address = data['results'][0]['formatted_address'];
+                // htmlData =  'Latitude : ' + latitude + '<br/>';
+                // htmlData += 'Longitude : ' + longitude + '<br/>';
+                // htmlData += 'Location : ' + formatted_address;
+                // $("#location").html('<a href="#" class="btn" onclick="drawMap('+latitude+', '+longitude+');">'+htmlData+'</a>');
+            // });
+        // }
  
-        function drawMap(latitude, longitude) {
-            var centerLocation = new google.maps.LatLng(latitude, longitude);
+        // function drawMap(latitude, longitude) {
+            // var centerLocation = new google.maps.LatLng(latitude, longitude);
  
-            var myOptions = {
-                center: centerLocation,
-                zoom: 16,
-                mapTypeId: google.maps.MapTypeId.ROADMAP
-            };
+            // var myOptions = {
+                // center: centerLocation,
+                // zoom: 16,
+                // mapTypeId: google.maps.MapTypeId.ROADMAP
+            // };
  
-            map_element = document.getElementById("map_canvas");
-            map = new google.maps.Map(map_element, myOptions);
+            // map_element = document.getElementById("map_canvas");
+            // map = new google.maps.Map(map_element, myOptions);
  
-            var marker = new google.maps.Marker({
-                position: centerLocation,
-                title: "My Current Location!"
-            });
-            marker.setMap(map);
+            // var marker = new google.maps.Marker({
+                // position: centerLocation,
+                // title: "My Current Location!"
+            // });
+            // marker.setMap(map);
  
-            var mapwidth = $(window).width();
-            var mapheight = $(window).height();
-            $("#map_canvas").height(mapheight);
-            $("#map_canvas").width(mapwidth);
-            google.maps.event.trigger(map, 'resize');
-        }
+            // var mapwidth = $(window).width();
+            // var mapheight = $(window).height();
+            // $("#map_canvas").height(mapheight);
+            // $("#map_canvas").width(mapwidth);
+            // google.maps.event.trigger(map, 'resize');
+        // }
  
-		$(function(){
-			$( ".btn" ).bind( "tap", tapHandler );
-			function tapHandler( event ){
-			$( event.target ).function(){
-			 navigator.geolocation.getCurrentPosition(success, error);
-			
-			}
-		});
+
 	
-};
+	};
